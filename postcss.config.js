0fe4e0753ede60fc850673a042ad270b3cc6744e
Developer Davo <DeveloperDavo@users.noreply.github.com>
2018-02-14 08:30:54 +0100

    Add cssnano and autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     plugins: [
         require('postcss-import')(),
+        require('autoprefixer')(),
+        require('cssnano')({preset: 'default'}),
         require('postcss-cssnext')(),
     ],
 };
